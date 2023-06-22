03d36ecd974bac9b1dfcfbf0e3d114b4f592d863
parosh <124004074@sastra.ac.in>
2023-06-23 02:49:33 +0530

    changing requirements

--- models.py
+++ models.py
@@ -27,7 +27,6 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
-    db.app = app
     moment.app = app
     db.init_app(app)
     db.create_all()
